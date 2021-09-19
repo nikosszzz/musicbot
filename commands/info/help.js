4678ef6c048917492403c9273f5e420fb32dd523
nikoszz <19nikospap@gmail.com>
2021-09-19 18:11:53 +0300

    commands: add help command

---
+++ commands/info/help.js
@@ -0,0 +1,30 @@
+const
+    { MessageEmbed } = require('discord.js');
+
+module.exports = {
+    config: {
+        name: 'help',
+        description: 'Displays the bot help page.',
+    },
+    execute: async (message) => {
+        let commands = message.client.commands.array();
+
+        let helpEmbed = new MessageEmbed()
+            .setColor('#000000')
+            .setTitle(`Info | Help`)
+            .setDescription(`Here are all the bot's commands.`)
+
+            commands.forEach((cmd) => {
+                helpEmbed.addField(
+                  `**${message.client.prefix}${cmd.config.name} ${cmd.config.aliases ? `(${cmd.config.aliases})` : ""}**`,
+                  `${cmd.config.description}`,
+                  true
+                );
+              });
+
+            helpEmbed.setFooter(message.member.displayName, message.author.displayAvatarURL({ dynamic: true }))
+            helpEmbed.setTimestamp()
+
+        await message.channel.send(helpEmbed).catch(console.error);
+    },
+};
